b5b21a52e21e8368aece4a99d2df2b754c8e5ffb
Luiz-Cunha-Dev <luizcunha.dev99@gmail.com>
2023-03-28 15:47:58 -0300

    feat: layout exit window

--- src/components/header.js
+++ src/components/header.js
@@ -12,9 +12,10 @@ import { ImExit } from "react-icons/im";
 export function Header() {
   const navigate = useNavigate();
   const [menu, setMenu] = useState(false);
+  const [exit, setExit] = useState(false);
 
   return (
-    <Container menu={menu === true ? "0px" : "-400px"}>
+    <Container menu={menu === true ? "0px" : "-400px"} exit={exit}>
       <div className="header">
         <HiOutlineMenu onClick={() => setMenu(!menu)} className="menu" />
         <div onClick={() => navigate("/")} className="icone">
@@ -29,34 +30,82 @@ export function Header() {
         <span>User Name</span>
 
         <div className="options">
-        <p onClick={() => navigate("/about-me")}>
-          <BsFillPersonFill className="icon"/>
-          Sobre Mim</p>
+          <p onClick={() => navigate("/about-me")}>
+            <BsFillPersonFill className="icon" />
+            Sobre Mim
+          </p>
 
-        <p onClick={() => navigate("/initial")} >
-          <FaHome className="icon"/>
-          Inicio</p>
+          <p onClick={() => navigate("/initial")}>
+            <FaHome className="icon" />
+            Inicio
+          </p>
 
-        <p onClick={() => navigate("/popular")}>
-          <AiFillFire className="icon"/>
-          Mais populares</p>
+          <p onClick={() => navigate("/popular")}>
+            <AiFillFire className="icon" />
+            Mais populares
+          </p>
 
-        <p onClick={() => navigate("/config")}>
-          <BsGearFill className="icon"/>
-          Configurações</p>
+          <p onClick={() => navigate("/config")}>
+            <BsGearFill className="icon" />
+            Configurações
+          </p>
 
-        <p onClick={() => navigate("/")}>
-          <ImExit className="icon"/>
-          Sair</p>
-
+          <p onClick={() => setExit(true)}>
+            <ImExit className="icon" />
+            Sair
+          </p>
+        </div>
+      </div>
+      <div onClick={() => {exit === true ? setMenu(menu) : setMenu(!menu)}} className="rigth-window">
+      <div className="exit-window">
+        <h2>Tem certeza de que quer sair da sua conta?</h2>
+        <button  onClick={() => navigate("/")}>Sair</button>
+        <p onClick={() => setExit(false)}>Cancelar</p>
       </div>
       </div>
-      <div onClick={() => setMenu(!menu)} className="rigth-window"></div>
     </Container>
   );
 }
 
 const Container = styled.div`
+    font-family: "Courier New", Courier, monospace;
+  .exit-window{
+    box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+    z-index: 11;
+    position: fixed;
+    width: 400px;
+    height: 350px;
+    background-color: white;
+    border-radius: 30px;
+    display: ${props => props.exit === true ? "flex" : "none"};
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    cursor: default;
+    button {
+      width: 160px;
+      height: 42px;
+      border-radius: 20px;
+      border: thin;
+      background-color: darkblue;
+      color: white;
+      font-size: 18px;
+      margin-top: 30px;
+      margin-bottom: 10px;
+      cursor: pointer;
+      transition: linear 0.1s;
+      :hover{
+        width: 170px;
+        height: 45px;
+        font-weight: bold;
+        font-size: 20px;
+      }
+    }
+    p{
+      cursor: pointer;
+    }
+  }
   .header {
     display: flex;
     align-items: center;
@@ -97,7 +146,6 @@ const Container = styled.div`
     background-color: darkblue;
     z-index: 2;
     color: white;
-    font-family: "Courier New", Courier, monospace;
     font-size: 30px;
     font-weight: bold;
     img {
@@ -114,27 +162,26 @@ const Container = styled.div`
       border-radius: 120px;
       margin-bottom: 25px;
     }
-    span{
-        margin-bottom: 50px;
-      }
+    span {
+      margin-bottom: 50px;
+    }
     .options {
       width: 65%;
       display: flex;
       flex-direction: column;
-
       font-size: 25px;
-    font-weight: 400;
-    p{
-      cursor: pointer;
-      display: flex;
-      align-items: center;
-      .icon{
-        font-size: 30px;
-        margin-right: 15px;
+      font-weight: 400;
+      p {
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+        .icon {
+          font-size: 30px;
+          margin-right: 15px;
+        }
       }
     }
-    }
-    .space{
+    .space {
       height: 2px;
       width: 80%;
       background-color: white;
@@ -148,7 +195,9 @@ const Container = styled.div`
     height: 100vh;
     background-color: rgba(10, 23, 55, 0.5);
     z-index: 1;
-    display: ${(props) => (props.menu === "0px" ? "initial" : "none")};
+    display: ${(props) => (props.menu === "0px" ? "flex" : "none")};
     cursor: pointer;
+    align-items: center;
+    justify-content: center;
   }
 `;
